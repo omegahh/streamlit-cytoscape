063077ad93cf19c7a1c785727fbeb675c52eb0d3
omegaxy <omeganju@gmail.com>
2025-08-24 20:24:06 +0800

    remove examples for not _RELEASE

--- st_cytoscape/__init__.py
+++ st_cytoscape/__init__.py
@@ -88,33 +88,3 @@ def cytoscape(
         default=default,
     )
     return component_value
-
-
-if not _RELEASE:
-    import streamlit as st
-
-    elements = [
-        {"data": {"id": "X"}, "selected": True, "selectable": False},
-        {"data": {"id": "Y"}},
-        {"data": {"id": "Z"}},
-        {"data": {"source": "X", "target": "Y", "id": "X➞Y"}},
-        {"data": {"source": "Z", "target": "Y", "id": "Z➞Y"}},
-        {"data": {"source": "Z", "target": "X", "id": "Z➞X"}},
-    ]
-
-    stylesheet = [
-        {"selector": "node", "style": {"label": "data(id)", "width": 20, "height": 20}},
-        {
-            "selector": "edge",
-            "style": {
-                "width": 3,
-                "curve-style": "bezier",
-                "target-arrow-shape": "triangle",
-            },
-        },
-    ]
-
-    selected = cytoscape(elements, stylesheet, key="graph")
-
-    st.markdown("**Selected nodes**: %s" % (", ".join(selected["nodes"])))
-    st.markdown("**Selected edges**: %s" % (", ".join(selected["edges"])))
